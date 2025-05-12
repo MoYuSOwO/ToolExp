72b25a1849f4ca982ce03e4b516762067f217360
MoYuSOwO <hhj2006@outlook.com>
2025-05-13 03:02:16 +0800

    feat: 更新api到v2.0.0

--- src/main/java/io/github/moyusowo/toolexp/ToolExp.java
+++ src/main/java/io/github/moyusowo/toolexp/ToolExp.java
@@ -1,11 +1,8 @@
 package io.github.moyusowo.toolexp;
 
 import com.destroystokyo.paper.event.player.PlayerPickupExperienceEvent;
-import io.github.moyusowo.neoartisanapi.api.attribute.AttributeRegistryAPI;
-import io.github.moyusowo.neoartisanapi.api.item.ItemRegistryAPI;
+import io.github.moyusowo.neoartisanapi.api.item.ItemRegistry;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
-import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,25 +13,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class ToolExp extends JavaPlugin implements Listener {
 
     private static NamespacedKey exp;
-    private static AttributeRegistryAPI attributeRegistry;
-    private static ItemRegistryAPI itemRegistry;
     private static final double base = 150.0;
 
     @Override
     public void onEnable() {
         exp = new NamespacedKey(this, "exp");
         getServer().getPluginManager().registerEvents(this, this);
-        attributeRegistry = Bukkit.getServicesManager().load(AttributeRegistryAPI.class);
-        itemRegistry = Bukkit.getServicesManager().load(ItemRegistryAPI.class);
-        if (attributeRegistry != null && itemRegistry != null) {
-            attributeRegistry.registerItemstackAttribute(exp, "double");
-        } else {
-            try {
-                throw new Exception("ERROR");
-            } catch (Exception e) {
-                getServer().getPluginManager().disablePlugin(this);
-            }
-        }
     }
 
     @EventHandler
@@ -42,14 +26,14 @@ public final class ToolExp extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         ItemStack hand = player.getInventory().getItemInMainHand();
         if (hand.getType().name().contains("SWORD")) {
-            if (itemRegistry.getItemstackAttributeValue(hand, exp) == null) {
+            if (ItemRegistry.getItemRegistryManager().getItemstackAttributeValue(hand, exp) == null) {
                 double expCount = base + event.getExperienceOrb().getExperience();
-                itemRegistry.setItemstackAttributeValue(hand, exp, expCount);
+                ItemRegistry.getItemRegistryManager().setItemstackAttributeValue(hand, exp, expCount);
                 player.sendMessage(Component.empty().content("当前武器经验: " + expCount));
             } else {
-                double expCount = itemRegistry.getItemstackAttributeValue(hand, exp);
+                double expCount = ItemRegistry.getItemRegistryManager().getItemstackAttributeValue(hand, exp);
                 expCount += event.getExperienceOrb().getExperience();
-                itemRegistry.setItemstackAttributeValue(hand, exp, expCount);
+                ItemRegistry.getItemRegistryManager().setItemstackAttributeValue(hand, exp, expCount);
                 player.sendMessage(Component.empty().content("当前武器经验: " + expCount));
             }
         }
