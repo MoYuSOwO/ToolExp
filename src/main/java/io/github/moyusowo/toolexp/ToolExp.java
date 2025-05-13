0dafe821831fe6cb5f403bd20bc93be35a4a128b
MoYuSOwO <hhj2006@outlook.com>
2025-05-13 19:53:32 +0800

    feat: 测试玩家属性运作

--- src/main/java/io/github/moyusowo/toolexp/ToolExp.java
+++ src/main/java/io/github/moyusowo/toolexp/ToolExp.java
@@ -2,25 +2,30 @@ package io.github.moyusowo.toolexp;
 
 import com.destroystokyo.paper.event.player.PlayerPickupExperienceEvent;
 import io.github.moyusowo.neoartisanapi.api.attribute.AttributeRegistry;
+import io.github.moyusowo.neoartisanapi.api.attribute.PlayerAttributeRegistry;
 import io.github.moyusowo.neoartisanapi.api.item.ItemRegistry;
 import net.kyori.adventure.text.Component;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ToolExp extends JavaPlugin implements Listener {
 
     private static NamespacedKey exp;
+    private static NamespacedKey attack;
     private static final double base = 150.0;
 
     @Override
     public void onEnable() {
         exp = new NamespacedKey(this, "exp");
+        attack = new NamespacedKey(this, "attack");
         getServer().getPluginManager().registerEvents(this, this);
         AttributeRegistry.getAttributeRegistryManager().registerItemstackAttribute(exp, "double");
+        PlayerAttributeRegistry.getPlayerAttributeRegistryManager().registerPlayerAttribute(attack, "int");
     }
 
     @EventHandler
@@ -37,6 +42,21 @@ public final class ToolExp extends JavaPlugin implements Listener {
                 expCount += event.getExperienceOrb().getExperience();
                 ItemRegistry.getItemRegistryManager().setItemstackAttributeValue(hand, exp, expCount);
                 player.sendMessage(Component.empty().content("当前武器经验: " + expCount));
+            }
+        }
+    }
+
+    @EventHandler
+    private void onPlayerAttack(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player player) {
+            if (PlayerAttributeRegistry.getPlayerAttributeRegistryManager().getPlayerAttribute(player, attack) == null) {
+                PlayerAttributeRegistry.getPlayerAttributeRegistryManager().setPlayerAttribute(player, attack, 1);
+                player.sendMessage(Component.empty().content("初始化记录"));
+            } else {
+                int times = PlayerAttributeRegistry.getPlayerAttributeRegistryManager().getPlayerAttribute(player, attack);
+                times++;
+                PlayerAttributeRegistry.getPlayerAttributeRegistryManager().setPlayerAttribute(player, attack, times);
+                player.sendMessage(Component.empty().content("玩家攻击次数: " + times));
             }
         }
     }
